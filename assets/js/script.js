9fb4fcdc13a8596babe624b448e86bc268c26852
Maeve Crossan <maevecrossan@gmail.com>
2024-06-27 11:16:40 +0000

    Add next game button and match button styles. Add inital JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,13 @@
 // Add event listeners
 
 document.addEventListener("DOMContentLoaded" , function() {
+    // Connect inputs to DOM
+    let player = document.getElementsByClassName('player');
+    console.log(player);
+
+    let boardSlot = document.getElementsByClassName('board-slot');
+    console.log(boardSlot);
+
     
 }
-
 )
