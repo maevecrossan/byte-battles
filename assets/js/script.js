e634b6cb9f7da495df868fa7a8f348ef6f63b027
Maeve Crossan <maevecrossan@gmail.com>
2024-06-27 14:44:26 +0000

    Readjust board HTML. Start adding event listeners

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,13 +3,22 @@
 
 document.addEventListener("DOMContentLoaded" , function() {
     // Connect inputs to DOM
-    let player = document.getElementsByClassName('player');
-    console.log(player);
 
-    let boardSlot = document.getElementsByClassName('board-slot');
-    console.log(boardSlot);
+    const board = document.getElementById('board');
+    
+    let boardSlot = document.querySelectorAll('data-slot');
+    
+    boardSlot.addEventListener('click', slotClick, {once:true});
 
     let nextRound = document.getElementById('next-round');
     let newGame = document.getElementById('new-game');
 }
 )
+
+/** 
+ * Registers clicks on game board slots
+ */
+
+function slotClick(event) {
+    console.log('clicked');
+}
